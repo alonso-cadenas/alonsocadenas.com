e6f4162ea7c011dd42a7d820e44345b8f5d03249
Alonso Cadenas <cadenas.alonso@gmail.com>
2025-02-28 02:25:02 -0600

    Add home styling and transitions

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 import "../css/modern-normalize.css";
-import "../css/style.css";
+import "../css/global.css";
+import "../css/home.css";
 // import { setupCounter } from "./counter.js";
 
 // setupCounter(document.querySelector("#counter"));
+
+window.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".section").classList.add("visible");
+});
